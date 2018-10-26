d1454447fb6ff0fc456b4dbaed8bef8573d444b9
Christopher Gunadi <christopher51333@gmail.com>
2018-10-26 13:32:48 +0100

    updated firebase config and implemented sign up functionaility in SignUpPage

--- CatchUp/constants/ApiKeys.js
+++ CatchUp/constants/ApiKeys.js
@@ -1,7 +1,10 @@
 export default {
   firebaseConfig: {
-    apiKey: 'AIzaSyAfRGN_oLvAOU0OhlBQ_ydNX6fRmnwFi7c',
-    authDomain: 'catchup-8a114.firebaseapp.com',
-    databaseURL: '',
+    apiKey: "AIzaSyAfRGN_oLvAOU0OhlBQ_ydNX6fRmnwFi7c",
+    authDomain: "catchup-8a114.firebaseapp.com",
+    databaseURL: "https://catchup-8a114.firebaseio.com",
+    projectId: "catchup-8a114",
+    storageBucket: "catchup-8a114.appspot.com",
+    messagingSenderId: "580091065854"
   },
 }
