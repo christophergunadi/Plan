03050988e37399e9e023eb4d33431d4ff9ccced2
Christopher Gunadi <christopher51333@gmail.com>
2018-10-26 01:37:40 +0100

    Created Input.js, to make more modular

--- CatchUp/components/LoginPage.js
+++ CatchUp/components/LoginPage.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
-import { 
-				StyleSheet, 
-				AppRegistry, 
-				Text, 
-				TextInput, 
-				View, 
-				Image, 
-        TouchableOpacity, 
+import {
+				StyleSheet,
+				AppRegistry,
+				Text,
+				TextInput,
+				View,
+				Image,
+        TouchableOpacity,
 } from 'react-native';
+import * as firebase from 'firebase';
+import { Input } from './Input';
 
-export default class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-		  username: '',
-			password: '',
-		};
-  }
+export default class LoginPage extends React.Component {
+	componentWillMount() {
+		const firebaseConfig = {
+			apiKey: 'AIzaSyAfRGN_oLvAOU0OhlBQ_ydNX6fRmnwFi7c',
+			authDomain: 'catchup-8a114.firebaseapp.com',
+		}
+
+		firebase.initializeApp(firebaseConfig);
+	}
+	
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+	// 	  username: '',
+	// 		password: '',
+	// 	};
+  // }
+
+	state = {
+		username: '',
+		password: '',
+	}
 
 	login() {
     //login api call
@@ -24,49 +40,37 @@ export default class LoginPage extends Component {
 
   render() {
     return (
-      <View>
-				<Image source={require('./logo.png')} />
-        <TextInput
-          style={styles.inputField}
-          placeholder="Username"
-          onChangeText={(username) => this.setState({username})}
-        />
-        <TextInput
-          style={styles.inputField}
-          placeholder="Password"
-          onChangeText={(password) => this.setState({password})}
-					secureTextEntry={true}
-        /> 
-			  <TouchableOpacity style = {styles.loginButton} onPress= {() => this.login()}>
-					  <Text style = {styles.loginText}> Login </Text>
+			<View>
+			  <Image source={require('./logo.png')} />
+				<Input
+					placeholder="Username"
+					label="Username"
+					onChangeText={(username) => this.setState({username})}
+					value={this.state.username}
+				/>
+				<Input
+					placeholder="Password"
+					label="Password"
+					onChangeText={(password) => this.setState({password})}
+					secureTextEntry
+					value={this.state.password}
+				/>
+				<TouchableOpacity style = {styles.loginButton} onPress= {() => this.login()}>
+					<Text style = {styles.loginText}> Login </Text>
 				</TouchableOpacity>
-      </View>
+			</View>
     );
   }
 }
 
 const styles = StyleSheet.create ({
-  inputField: {
-    height: 40,	
-		width: 250,
-		borderWidth: 1,
-		borderColor: '#CDC6C6',
-		borderBottomLeftRadius: 8,
-	  borderBottomRightRadius: 8,
-	  borderTopLeftRadius: 8,
-	  borderTopRightRadius: 8,
-		padding: 5,
-		margin: 5,
-	},
 	loginButton: {
   	backgroundColor: '#CDC6C6',
-  	// width: 150,
   	height: 40,
     margin: 5,
-  	borderRadius: 30,	
+  	borderRadius: 30,
 		alignItems: 'center',
 		justifyContent:'center',
-
 	},
 	loginText: {
 		color: 'black',
